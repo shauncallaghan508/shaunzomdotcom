eeacae8a5116503e4b6fd042b870185790293029
Brandon Aaskov <brandonaaskov@gmail.com>
2020-01-21 09:31:26 -0500

    switches to using sass
    You'll probably want to migrate your fonts and whatnot into the sass folder too.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
 import Router from "./components/Router";
-import './css/style.css';
+import './sass/main.scss';
 
 render(<Router />, document.querySelector('#main'));
